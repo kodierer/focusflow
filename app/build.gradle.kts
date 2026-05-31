67f68dc07edd68ea582afbc2f9dd43f7a5741b0f
kodierer <kodierer@github.com>
2026-05-31 23:47:11 +0200

    Bump versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.kodierer.focusflow"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
